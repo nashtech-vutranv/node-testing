40fc41c6de2598ff5bbbd37164c6c48bdbe574eb
Vu Tran <vutranv@nashtechglobal.com>
2023-02-09 23:34:07 +0700

    Create mongodb schema, init get, post, update method

--- controller/posts.js
+++ controller/posts.js
@@ -1,3 +1,40 @@
-export const getPost = (req, res) => {
-  res.send('ROUTER SUCCESS')
+import {PostModel} from '../models/PostModel.js'
+
+export const getPost = async (req, res) => {
+  try {
+    const post = new PostModel({
+      title: 'test',
+      content: 'test'
+    })
+
+    post.save()
+
+    const posts = await PostModel.find()
+    console.log({posts})
+    res.status(200).json(posts)
+  } catch (err) {
+    res.status(500).json({err})
+  }
+}
+
+export const createPost = async (req, res) => {
+  try {
+    const newPost = req.body
+    const post = new PostModel(newPost)
+    await post.save()
+    res.status(200).json(post)
+  } catch(err) {
+    res.status(500).json({err})
+  }
+} 
+
+export const updatePost = async (req, res) => {
+  try {
+    const updatePost = req.body
+    const post = new PostModel.findOneAndUpdate({_id: updatePost._id}, updatePost, {new: true})
+    
+    res.status(200).json(post)
+  } catch(err) {
+    res.status(500).json({err})
+  }
 }
--- router/posts.js
+++ router/posts.js
@@ -1,8 +1,12 @@
 import express from 'express'
-import {getPost} from '../controller/posts.js'
+import {getPost, createPost, updatePost} from '../controller/posts.js'
 
 const router = express.Router()
 
 router.get('/', getPost)
+
+router.post('/', createPost)
+
+router.post('/update', updatePost)
 
 export default router
