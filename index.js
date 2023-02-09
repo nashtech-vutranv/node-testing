8bd8e3ab7f33fe739eeabdc69e243499f4375ddc
Vu Tran <vutranv@nashtechglobal.com>
2023-02-09 23:05:35 +0700

    Setup mongoDB

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
-import post from './router/post.js'
+import post from './router/posts.js'
+import mongoose from 'mongoose'
 
 const app = express()
 const PORT = process.env.port || 5000
+
+const URI = 'mongodb+srv://vutran:PukLygC1ZgIBVIHk@cluster0.tbz2zk0.mongodb.net/?retryWrites=true&w=majority'
 
 app.use(bodyParser.json({limit: '30mb'}))
 app.use(bodyParser.urlencoded({extended: true, limit: '30mb'}))
@@ -12,6 +15,14 @@ app.use(cors())
 
 app.use('/posts', post)
 
-app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`)
+mongoose.connect(URI).then(
+  () => {
+    console.log('connected to DB')
+    app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`)
 })
+  }
+).catch(err => {
+  console.log({err})
+})
+
--- router/posts.js
+++ router/posts.js
@@ -1,9 +1,8 @@
 import express from 'express'
+import {getPost} from '../controller/posts.js'
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.send('ROUTER SUCCESS')
-})
+router.get('/', getPost)
 
 export default router
